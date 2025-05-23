04714f4f6fe566f00287df9334d2f3d4c8edb260
vi-tr <144982881+vi-tr@users.noreply.github.com>
2025-05-23 04:23:41 -0500

    Easy fix

--- rust-components/src/lib.rs
+++ rust-components/src/lib.rs
@@ -1,14 +1,6 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+use godot::prelude::*;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+struct MyExtension;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+#[gdextension]
+unsafe impl ExtensionLibrary for MyExtension {}
